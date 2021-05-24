1c652628f5285f28557d69b119d39f3e4a203a15
HarishGokul-15 <harish15gokul@gmail.com>
2021-05-24 10:49:51 +0530

    splash screen

---
+++ app/src/main/java/Hospital/DetailsHospitals.java
@@ -0,0 +1,18 @@
+package Hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covidcare.R;
+
+public class DetailsHospitals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_hospitals);
+
+
+    }
+}
--- app/src/main/java/com/example/covidcare/HomeActivity.java
+++ app/src/main/java/com/example/covidcare/HomeActivity.java
@@ -2,7 +2,12 @@ package com.example.covidcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import Hospital.RandL;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView hospital = (ImageView) findViewById(R.id.hospital);
+
+        hospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RandL.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
