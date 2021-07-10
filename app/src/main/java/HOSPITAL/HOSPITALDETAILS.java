eebd1366164dce449b182d30206fee132e8bfb0f
HarishGokul-15 <harish15gokul@gmail.com>
2021-07-10 10:29:16 +0530

    First Commit

---
+++ app/src/main/java/HOSPITAL/HOSPITALDETAILS.java
@@ -0,0 +1,16 @@
+package HOSPITAL;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covidcare.R;
+
+public class HOSPITALDETAILS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospitaldetails);
+    }
+}
---
+++ app/src/main/java/USER/NavigationActivity.java
@@ -0,0 +1,16 @@
+package USER;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covidcare.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
