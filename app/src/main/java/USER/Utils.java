d20ccb2ed27e8b957441ff83cb9b043263549a60
ProjctConnect <83457447+ProjctConnect@users.noreply.github.com>
2021-11-20 18:47:01 +0530

    Delete Utils.java

--- app/src/main/java/USER/Utils.java
+++
@@ -1,8 +0,0 @@
-package USER;
-
-public class Utils {
-    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
-
-    public static final  String EMAIL = "covidcareofficial2021@gmail.com";
-    public static final String PASSWORD = "covid@2002";
-}
